c0a9150781da10cc99d4ae71e8d6cd4a135ec967
Janette <janette.castillo91@yahoo.com>
2022-02-28 22:18:59 -0600

    added contact info to footer section

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -16,8 +16,11 @@ export default function Footer(){
                     <a target="_blank" href="resume.pdf">
                         <img src={resume} id="icon"/>
                         </a>
-
-                        <p>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a
+                        <p>Email:
+            <a href="mailto:janette.castillo91@yahoo.com"><strong>janette.castillo91@yahoo.com</strong></a> Phone:
+            <strong>(312) 543-2191</strong>
+        </p>
+            <p>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a
                 href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></p>
     </footer>
     )
