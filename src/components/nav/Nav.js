df1ba1c4e28b86fb04e0ffb71cc0158a0d2667fe
Janette <janette.castillo91@yahoo.com>
2022-03-02 14:17:56 -0600

    removed hash router

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,4 +1,5 @@
 import { Container, Nav, Navbar, Offcanvas } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 export default function NavLinks() {
     return (
@@ -17,10 +18,10 @@ export default function NavLinks() {
                     </Offcanvas.Header>
                     <Offcanvas.Body>
                         <Nav className="justify-content-end flex-grow-1 pe-3">
-                            <Nav.Link href="/">About Me</Nav.Link>
-                            <Nav.Link href="/portfolio">Portfolio</Nav.Link>
-                            <Nav.Link href="/contact">Contact</Nav.Link>
-                            <Nav.Link href="/resume">Resume</Nav.Link>
+                            <Link href="/">About Me</Link>
+                            <Link href="/portfolio">Portfolio</Link>
+                            <Link href="/contact">Contact</Link>
+                            <Link href="/resume">Resume</Link>
                         </Nav>
                     </Offcanvas.Body>
                 </Navbar.Offcanvas>
