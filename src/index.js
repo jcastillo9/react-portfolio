f81d12ef4ff15d928ded5591bc5dbb019952c16c
Janette <janette.castillo91@yahoo.com>
2022-03-03 21:46:20 -0600

    removed package

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter} from 'react-router-dom'
+import { HashRouter} from 'react-router-dom'
 import './style.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -11,9 +11,9 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
