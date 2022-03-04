69dd67f91ec82d19155543ab31563f083f7ba196
Janette <janette.castillo91@yahoo.com>
2022-03-03 19:24:02 -0600

    deploy

--- src/index.js
+++ src/index.js
@@ -4,16 +4,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import reportWebVitals from './reportWebVitals';
 import './style.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
-   <HashRouter>
+   <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
