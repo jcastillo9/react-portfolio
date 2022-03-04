d5e292f1394a1cecae317f4a2cad05949264bf38
Janette <janette.castillo91@yahoo.com>
2022-03-03 20:16:21 -0600

    working with routes

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import { HashRouter } from 'react-router-dom'
 
 ReactDOM.render(
   <React.StrictMode>
-   <HashRouter>
+   <HashRouter basename="/">
       <App />
     </HashRouter>
   </React.StrictMode>,
