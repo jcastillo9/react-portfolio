b5949829fa453cfadd53bd2d301ab72922f7f113
Janette <janette.castillo91@yahoo.com>
2022-03-03 20:52:18 -0600

    changed routes again

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {Router, Route, Switch} from 'react-router-dom';import './style.css';
+import {Routes, Route } from 'react-router-dom';import './style.css';
 import Header from './components/header/Header'
 import About from './components/about/About'
 import Portfolio from './components/portfolio/Portfolio'
@@ -10,24 +10,14 @@ import Footer from './components/footer/Footer'
 function App() {
   return (
     <div>
-      <Router>
       <Header />
-      <Switch>  
+      <Routes>  
         <Route exact path="/" element={<About />} />
         <Route path="/portfolio" element={<Portfolio />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/resume" element={<Resume />} />
-      </Switch>
-      <Footer />
-        </Router>
-      {/* <Header />
-      <Routes>  
-        <Route exact path='react-portfolio-v4/' element={<About />} />
-        <Route exact path='react-portfolio-v4/portfolio' element={<Portfolio />} />
-        <Route exact path='react-portfolio-v4/contact' element={<Contact />} />
-        <Route exact path='react-portfolio-v4/resume' element={<Resume />} />
       </Routes>
-      <Footer /> */}
+      <Footer />
     </div>
   );
 }
